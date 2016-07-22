cbce169086e6e1693f80c6d36aa80239646c8fd7
kmfb21 <kmfb21@gmail.com>
2016-07-21 21:30:39 -0400

    install Firebase

---
+++ RemoteMac/func.swift
@@ -0,0 +1,50 @@
+//
+//  func.swift
+//  Samples
+//
+//  Created by 博 方 on 16/5/17.
+//  Copyright © 2016年 博 方. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+let BASE_URL = "blistering-heat-225.firebaseio.com"
+let FIREBASE_REF = Firebase(url: BASE_URL)
+
+var CURRENT_USER: Firebase {
+    let userID = NSUserDefaults.standardUserDefaults().valueForKey("uid") as! String
+    let currentUser = Firebase(url: "\(FIREBASE_REF)").childByAppendingPath("users").childByAppendingPath(userID)
+    return currentUser!
+}
+
+func Alert(mes: String) -> UIAlertController {
+    let alert = UIAlertController(title: "Alert", message: mes, preferredStyle: .Alert)
+    let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+    alert.addAction(action)
+    return alert
+}
+//********************************User********************************
+func IfUserLoggedIn() -> Bool {
+    print("******Checking User Logged In:********")
+    print(NSUserDefaults.standardUserDefaults().valueForKey("uid"))
+    if NSUserDefaults.standardUserDefaults().valueForKey("uid") != nil {
+        print(CURRENT_USER.authData)
+    }
+    print("***************Data Ends**************")
+    return NSUserDefaults.standardUserDefaults().valueForKey("uid") != nil && CURRENT_USER.authData != nil
+}
+func SetUserLogout() {
+    CURRENT_USER.unauth()
+    NSUserDefaults.standardUserDefaults().setValue(nil, forKey: "uid")
+    print("Logged out")
+}
+func SetUserLogin(authData: FAuthData) {
+    NSUserDefaults.standardUserDefaults().setValue(authData.uid, forKey: "uid")
+    print("Logged in")
+}
+
+
+
+
+
---
+++ RemoteMac/iTunesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  iTunesViewController.swift
+//  RemoteMac
+//
+//  Created by 博 方 on 16/7/21.
+//  Copyright © 2016年 博 方. All rights reserved.
+//
+
+import UIKit
+
+class iTunesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
